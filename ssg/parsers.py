6d5110cc1b8963e34ac1b1ef0b0920b45268d92e
dvictoria2 <dvictoria2@slb.com>
2023-01-19 09:39:56 -0500

    Update parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -11,8 +11,6 @@ from ssg.content import Content
 from ssg import hooks
 
 
-from ssg import hooks
-
 class Parser:
     base_ext = ".html"
     file_exts: List[str] = []
@@ -49,7 +47,9 @@ class MarkdownParser(Parser):
     def parse(self, path, source, dest):
         content = Content.load(self.read(path))
         html = markdown(content.body)
-        self.write(path, dest, html)
+        #self.write(path, dest, html)
+        filtered = hooks.filter("generate_menu", html, self.base_ext)
+        self.write(path, dest, filtered)
         sys.stdout.write(
             "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
         )
@@ -61,7 +61,9 @@ class ReStructuredTextParser(Parser):
     def parse(self, path, source, dest):
         content = Content.load(self.read(path))
         html = publish_parts(content.body, writer_name="html5")
-        self.write(path, dest, html["html_body"])
+       # self.write(path, dest, html["html_body"])
+        filtered = hooks.filter("generate_menu", html["html_body"], self.base_ext)
+        self.write(path, dest, filtered)
         sys.stdout.write(
             "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
         )
